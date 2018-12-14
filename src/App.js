5933ba79e36d3c44091fdf9a8e3fcf4b46cf82e4
td94 <quynhtrangdo94@gmail.com>
2018-12-14 02:13:59 -0500

    another attemp at Modal

--- src/App.js
+++ src/App.js
@@ -10,19 +10,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
+  state = {
+    showResult: false
+  }
+  toggleModal = () => this.setState({
+    showResult: !this.state.showResult
+  })
   render() {
     return (
-      <Provider store={ store }>
+      <Provider store={store}>
         <div className="App bg-light">
-          <Header/>
-          <Score/>
-          <Question/>
-          <Result/>
-          <Footer/>
+          <Header />
+          <Score />
+          <Question />
+          <div className={`container ${this.state.showResult ? 'modal-open' : ''}`}>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-9">{' '}</div>
+                <div className="col-md-3 d-flex justify-content-center">
+                  <button type="button"
+                    className="btn btn-success m-3 d-none Result"
+                    data-toggle="modal" data-target="#resultModal"
+                    onClick = { this.toggleModal }
+                  >Result</button>
+                </div>
+              </div>
+            </div>
+            <Result toggle = {this.toggleModal} showModal={this.state.showModal}/>
+          </div>
+          <Footer />
         </div>
       </Provider>
     );
   }
 }
+
 
 export default App;
--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
     return {
-        score: state.scoreInfo.score
+        score: state.scoreInfo.score,
+        totalQuestion: state.scoreInfo.totalQuestion
     }
 }
 
@@ -14,29 +15,47 @@ const mapDispatchToProps = dispatch => {
 
 
 class Result extends Component {
+    componentDidMount() {
+        //open modal
+    }
+    dismissModal = () => {
+        this.props.toggle()
+    }
     render() {
-        const { score } = this.props;
+        const { score,totalQuestion } = this.props;
         return (
             <div>
-                <div className="container">
+                {/* <div className="container">
                     <div className="row">
                         <div className="col-md-9">{' '}</div>
                         <div className="col-md-3 d-flex justify-content-center">
-                            <button type="button" className="btn btn-success m-3 d-none Result" data-toggle="modal" data-target="#resultModal">Result</button>
+                            <button type="button" 
+                                className="btn btn-success m-3 d-none Result" 
+                                data-toggle="modal" data-target="#resultModal"
+                            >Result</button>
                         </div>
                     </div>
-                </div>
-                <div className="modal fade" id="resultModal" role="dialog">
+                </div> */}
+                <div onClick={this.dismissModal}
+                    className={`modal fade WelcomeModal ${this.props.showModal ? 'show' : ''}`} 
+                    style={{
+                        display: `${this.props.showModal ? 'block' : 'none'}`
+                    }}
+                    id="resultModal" 
+                    tabIndex="-1" 
+                    role="dialog" 
+                    aria-labelledby="resultModalCenterTitle" 
+                    aria-hidden="true">
                     <div className="modal-dialog modal-dialog-centered" role="document">
                         <div className="modal-content">
                             <div className="modal-header">
                                 <h5 className="modal-title">Your Result</h5>
-                                <button type="button" className="close" data-dismiss="modal">
-                                    <span> &times; </span>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true"> &times; </span>
                                 </button>
                             </div>
                             <div className="modal-body">
-                                <p>You got {Number(score)} out of 5 flags correctly</p>
+                                <p>You got {Number(score)} out of {Number(totalQuestion)}  flags correctly</p>
                             </div>
                             <div className="modal-footer">
                                 <button className="btn btn-warning btn-block">Restart</button>
@@ -48,4 +67,5 @@ class Result extends Component {
         );
     }
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(Result);
