b6a73793ed8cda23f279bf102ca56da5fdb944bd
td94 <quynhtrangdo94@gmail.com>
2018-12-14 20:41:49 -0500

    change game name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="row">
             <div className="col py-2 mt-3 bg-warning text-danger text-center">
-                <h3>Guess The Flag</h3>
+                <h3>Guess That Flag</h3>
                 <p>Copyright of Trang Do  &copy; 2018</p>
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
             <div className="col py-5 mb-3 bg-warning text-danger text-center">
                 <h1>
                     <i className="fas fa-flag mr-3"/>
-                    <strong>Guess The Flag!</strong>
+                    <strong>Guess That Flag!</strong>
                 </h1>
             </div>
         </div>
