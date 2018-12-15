a5fd4a6330baa53b6d9f03b8a10b48b36c27f56d
td94 <quynhtrangdo94@gmail.com>
2018-12-14 20:53:52 -0500

    update header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,11 @@ const Footer = () => {
         <div className="row">
             <div className="col py-2 mt-3 bg-warning text-danger text-center">
                 <h3>Guess That Flag</h3>
-                <p>Copyright of Trang Do  &copy; 2018</p>
+                <p>Copyright of {' '}
+                    <span>
+                        <a href="https://github.com/tdo94" className="text-success">Trang Do</a>
+                    </span>
+                    {' '} &copy; 2018</p>
             </div>
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="row">
-            <div className="col py-5 mb-3 bg-warning text-danger text-center">
+            <div className="col py-4 mb-3 bg-warning text-danger text-center">
                 <h1>
                     <i className="fas fa-flag mr-3"/>
                     <strong>Guess That Flag!</strong>
                 </h1>
+                <h4 className="text-success">You have 5 chances. Go!</h4>
             </div>
         </div>
     )
